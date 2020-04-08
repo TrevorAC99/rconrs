3fcadec72a2b5b29bc16bf169fe23a24fffe70dc
Trevor Carlson <trevorac99@gmail.com>
2020-04-08 00:59:55 -0500

    reorganize main.rs into multiple smaller functions

--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,20 @@
-use std::io::prelude::*;
+use std::io::{stdin, stdout, prelude::*};
+use rcon::RconClient;
 
 mod rcon;
 
 fn main() {
-    let input = std::io::stdin();
-    let mut input = input.lock();
-
     println!("{}", include_str!("greeting.txt"));
 
-    print!("Enter the host you wish to connect to or leave blank for localhost: ");
-    std::io::stdout().flush().unwrap();
+    let host = read_host();
 
-    let mut host_buffer = String::new();
-    input.read_line(&mut host_buffer).unwrap();
+    let port = read_port();
 
-    let host = if host_buffer.trim().eq("") {
-        "localhost"
-    } else {
-        host_buffer.trim()
-    };
-
-    let mut port_buffer = String::new();
-
-    print!("Enter the port you wish to use or leave blank for 25575: ");
-    std::io::stdout().flush().unwrap();
-    let port = loop {
-        input.read_line(&mut port_buffer).unwrap();
-        let port_trimmed = port_buffer.trim();
-
-        if port_trimmed.is_empty() { break 25575 }
-
-        match port_trimmed.parse::<u16>() {
-            Ok(port) => { break port }
-            Err(_) => {
-                print!("Unable to parse input. Enter the port you wish to use or leave blank for 25575: ");
-                std::io::stdout().flush().unwrap();
-                continue
-            }
-        }
-    };
-
-    print!("Enter the password: ");
-    std::io::stdout().flush().unwrap();
-
-    let mut password_buffer = String::new();
-    input.read_line(&mut password_buffer).unwrap();
-
-    let password = password_buffer.trim();
+    let password = read_password();
 
     print!("Attempting to connect... ");
 
-    let mut client = match rcon::RconClient::connect(host, port, password) {
+    let client = match rcon::RconClient::connect(&host, port, &password) {
         Err(_) => {
             println!("Unable to authenticate. Did you enter your password correctly? Shutting down.");
             return;
@@ -60,9 +24,68 @@ fn main() {
 
     println!("Authentication successful");
 
+    command_loop(client);
+}
+
+/// Asks the user for a host name and reads it into a string.
+fn read_host() -> String {
+    print!("Enter the host you wish to connect to or leave blank for localhost: ");
+    stdout().flush().unwrap();
+
+    let mut host_buffer = String::new();
+    stdin().read_line(&mut host_buffer).unwrap();
+
+    let host = if host_buffer.trim().eq("") {
+        "localhost"
+    } else {
+        host_buffer.trim()
+    };
+
+    String::from(host)
+}
+
+/// Asks the user for a port number and reads it into a u16.
+fn read_port() -> u16 {
+    let mut port_buffer = String::new();
+
+    print!("Enter the port you wish to use or leave blank for 25575: ");
+    stdout().flush().unwrap();
+
+    loop {
+        stdin().read_line(&mut port_buffer).unwrap();
+        let port_trimmed = port_buffer.trim();
+
+        if port_trimmed.is_empty() { break 25575 }
+
+        match port_trimmed.parse::<u16>() {
+            Ok(port) => { break port }
+            Err(_) => {
+                print!("Unable to parse input. Enter the port you wish to use or leave blank for 25575: ");
+                stdout().flush().unwrap();
+                continue
+            }
+        }
+    }
+}
+
+/// Asks the user for a password and reads it into a String.
+fn read_password() -> String {
+    print!("Enter the password: ");
+    stdout().flush().unwrap();
+
+    let mut password_buffer = String::new();
+    stdin().read_line(&mut password_buffer).unwrap();
+
+    String::from(password_buffer.trim())
+}
+
+/// Takes commands from the user and executes them as commands until the user
+/// executes the ```quit``` command.
+fn command_loop(mut client: RconClient) {
+    let input = stdin();
     loop {
         print!("Enter a command: ");
-        std::io::stdout().flush().unwrap();
+        stdout().flush().unwrap();
         
         let mut command_buffer = String::new();
         input.read_line(&mut command_buffer).unwrap();
